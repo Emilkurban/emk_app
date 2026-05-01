fa1b34f840e5f10eb5665ad7b23767e970ebd456
Emilkurban <emkurr@yandex.ru>
2026-05-01 23:23:39 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,6 @@ class _WebViewScreenState extends State<WebViewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Добавляем отступ сверху, чтобы контент не заезжал под статус бар
     return Scaffold(
       body: SafeArea(
         child: Stack(
@@ -67,17 +66,6 @@ class _WebViewScreenState extends State<WebViewScreen> {
               const Center(
                 child: CircularProgressIndicator(),
               ),
-            Positioned(
-              bottom: 20,
-              right: 20,
-              child: FloatingActionButton(
-                onPressed: () {
-                  controller.reload();
-                  setState(() => isLoading = true);
-                },
-                child: const Icon(Icons.refresh),
-              ),
-            ),
           ],
         ),
       ),
