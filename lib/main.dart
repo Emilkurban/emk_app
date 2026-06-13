e1b3e0f0c76fd2ab49a6c2ef748f343ffd766f15
Emilkurban <emkurr@yandex.ru>
2026-06-13 19:58:30 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'EMK',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
       home: const WebViewScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -32,64 +26,19 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController controller;
-  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0xFFFFFFFF))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (url) {
-            setState(() => isLoading = true);
-          },
-          onPageFinished: (url) {
-            setState(() => isLoading = false);
-          },
-          onWebResourceError: (error) {
-            print('Error: ${error.description}');
-            setState(() => isLoading = false);
-          },
-          onNavigationRequest: (request) {
-            if (request.url.startsWith('tel:')) {
-              _launchUrl(request.url);
-              return NavigationDecision.prevent;
-            }
-            if (request.url.startsWith('https://wa.me/')) {
-              _launchUrl(request.url);
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
       ..loadRequest(Uri.parse('https://emk.az'));
-  }
-
-  Future<void> _launchUrl(String url) async {
-    final Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            WebViewWidget(controller: controller),
-            if (isLoading)
-              const Center(
-                child: CircularProgressIndicator(),
-              ),
-          ],
-        ),
-      ),
+      body: WebViewWidget(controller: controller),
     );
   }
 }
