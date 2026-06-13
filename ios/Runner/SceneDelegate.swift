9f3a99e7c1a07c57af9bce308e54317970f06fa4
Emilkurban <emkurr@yandex.ru>
2026-06-13 18:40:10 +0400

    Update SceneDelegate.swift

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,8 @@ import UIKit
 import Flutter
 
 class SceneDelegate: FlutterSceneDelegate {
-    // Стандартная реализация
+    override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        (UIApplication.shared.delegate as! FlutterAppDelegate).window = window
+        super.scene(scene, willConnectTo: session, options: connectionOptions)
+    }
 }
